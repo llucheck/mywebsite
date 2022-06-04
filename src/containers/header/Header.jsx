7a8bfdafc194f086c43e3eef4951e37b657d2bc8
leonard <leonardlucheck@gmail.com>
2022-06-04 15:23:01 -0500

    Changed color scheme and added header content
    changed the color scheme so text was more readable, added content to header and added css for current header content

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,27 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <div className='websiteHeader sectionPadding' id="home">
+      <div className='websiteHeaderContent'>
+        <h1>Welcome to my first actual website!</h1>
+
+        <p>
+          Hi, my name is Leonard. I'm a recent graduate from Lewis 
+          University with a Bachelors of Science in Computer Science.
+          There I learned many new coding practices and coding languages.
+          Some examples are Java, HTML, Javascript, CSS, React.js, and 
+          C++. I love to code in general and I've been doing it since
+          highschool. I'm starting my coding journey by trying to find an 
+          exciting and well-paced job.
+        </p>
+
+        <p>
+          Follow me on my coding journey as I develop new projects and 
+          work hard to develop this website at my github <a href="https://github.com/llucheck">Here!</a> 
+        </p>
+
+      </div>
+    </div>
   )
 }
 
